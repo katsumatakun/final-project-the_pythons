b634a612e535c1982709f0343868ed40ee543eac
Katie Chace <klchace@coe.edu>
2019-04-09 09:00:44 -0500

    added Masa's explainations from Slack as comments

--- cmp_extension.c
+++ cmp_extension.c
@@ -1,6 +1,9 @@
 #include <string.h>
 #include "cmp_extension.h"
 
+/* This function checks if a string (*words) ends with a string (*word)
+ This allow us to check if a file name input has ".dsk" extension. */
+
 int contain(char *word, char *words){
   char *s = words;
   int pos = strlen(s) - strlen(word) - 1;
--- cmp_extension.h
+++ cmp_extension.h
@@ -1,1 +1,3 @@
 int contain(char*, char*);
+/* This function checks if a string (*words) ends with a string (*word)
+ This allow us to check if a file name input has ".dsk" extension. */
