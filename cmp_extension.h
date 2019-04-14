5b2ae264e9c22a7735bb25be398973f5a7facb64
Zach Kammin <zpkammin@coe.edu>
2019-04-14 15:42:04 -0500

    Added include guards to the header files

--- cmp_extension.h
+++ cmp_extension.h
@@ -1,3 +1,9 @@
+#ifndef CMP_EXTENSION
+#define CMP_EXTENSION
+
 int contain(char*, char*);
 /* This function checks if a string (*words) ends with a string (*word)
  This allow us to check if a file name input has ".dsk" extension. */
+
+
+#endif
