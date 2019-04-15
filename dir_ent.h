fe37770337003c0b5731cbc328ab3b0ed38c7d0d
MasahiroKiji <mkiji@coe.edu>
2019-04-15 15:06:41 -0500

    change the name of union

--- dir_ent.h
+++ dir_ent.h
@@ -1,19 +1,19 @@
 #ifndef _DIR_ENT_
 #define _DIR_ENT_
 
-typedef struct directly_entry* entPtr;
+typedef struct directory_entry* entPtr;
 
 struct floppy_alloc {
   unsigned short alloc_1:8;
   unsigned short alloc_2:8;
 };
 
-union alloc{
+union alloc_block{
   struct floppy_alloc f_alloc;
   unsigned short h_alloc;
 };
 
-struct directly_entry{
+struct directory_entry{
     unsigned char status;
     char name[8];
     char extension[3];
@@ -21,14 +21,14 @@ struct directly_entry{
     unsigned char bc;
     unsigned char xh;
     unsigned char rc;
-    union alloc alc1;
-    union alloc alc2;
-    union alloc alc3;
-    union alloc alc4;
-    union alloc alc5;
-    union alloc alc6;
-    union alloc alc7;
-    union alloc alc8;
+    union block_alloc alc1;
+    union block_alloc alc2;
+    union block_alloc alc3;
+    union block_alloc alc4;
+    union block_alloc alc5;
+    union block_alloc alc6;
+    union block_alloc alc7;
+    union block_alloc alc8;
   };
 
 #endif
