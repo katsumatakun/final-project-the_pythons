1f46a2fe829a1f8c05410ba3f0a3c451a833a784
Nathan <nsolsen@coe.edu>
2019-04-15 19:41:48 -0500

    Adding cpmremove function

---
+++ cpmremove.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "cmp_extension.h"
+#include "node.h"
+
+nodePtr remove(char* filepath, char opt){
+
+
+}
---
+++ cpmremove.h
@@ -0,0 +1,9 @@
+#ifndef _remove_
+#define _remove_
+
+#include "node.h"
+
+nodeptr remove(char* filepath, char opt);
+
+
+#endif
