1fcb68de92513b583dec1892814f767b48af5e59
Katie Chace <klchace@coe.edu>
2019-05-06 16:37:12 -0500

    fixed references to block_alloc

--- findFreeSpace.c
+++ findFreeSpace.c
@@ -13,10 +13,13 @@ int* findFreeSpace(nodePtr head){
 
 
   while(head->next != NULL){
-    ba = head->directory_entry->blocks;
+    ba = head->ptr->blocks;
 
     for(int i = 0; i < blocks_per_dir[disk_type]; i++){
-      location = ba[i];
+      if(disk_type == FLOPPY)
+        location = ba.floppy_block[i];
+      else
+        location = ba.hard_block[i];
       if(location == 0) /* if the end of file has been reached */
         i = blocks_per_dir[disk_type]; /* end the loop */
       else
