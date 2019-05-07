d9fd66e03f711a2f8bdc2c2170fa09d076b1e230
Katie Chace <klchace@coe.edu>
2019-05-06 20:29:31 -0500

    putting disk.h back together

--- disk.c
+++
@@ -1,8 +0,0 @@
-#include "disk.h"
-
-num_of_blocks[2] = {244, 2040};
-/* 77 - 2 * 26 / 8  for floppy*/
-/* 255 * 128 / 16  for hard drive */
-/* number of tracks * sectors per tract / sectors per block */
-
-int blocks_per_dir[2] = {8,16};
--- disk.h
+++ disk.h
@@ -7,12 +7,12 @@
 int disk_type;
 
 
-int num_of_blocks[2];
+int num_of_blocks[2] = {244, 2040};
 /* 77 - 2 * 26 / 8  for floppy*/
 /* 255 * 128 / 16  for hard drive */
 /* number of tracks * sectors per tract / sectors per block */
 
-int blocks_per_dir[2];
+int blocks_per_dir[2] = {8,16};
 
 
 
