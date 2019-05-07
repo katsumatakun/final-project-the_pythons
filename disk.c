d7685ffe043c26859569a0d904b59e6072f76073
Katie Chace <klchace@coe.edu>
2019-05-06 19:47:44 -0500

    fixed bug from redefinitions from disk.h

--- disk.c
+++ disk.c
@@ -1,20 +1,8 @@
-#ifndef DISK
-#define DISK
+#include "disk.h"
 
-#define FLOPPY 0
-#define HARD 1
-
-int disk_type;
-
-
-int num_of_blocks[2];
+num_of_blocks[2] = {75 * 26 / 8, 255 * 128 / 16};
 /* 77 - 2 * 26 / 8  for floppy*/
 /* 255 * 128 / 16  for hard drive */
 /* number of tracks * sectors per tract / sectors per block */
 
-int blocks_per_dir[2];
-
-
-
-
-#endif
+int blocks_per_dir[2] = {8,16};
